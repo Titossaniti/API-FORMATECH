958eb11898456a0aa6a7d2c35e2259ec5e531f6c
Titossaniti <tristan.erard@gmail.com>
2024-09-23 16:01:40 +0200

    ReadMe

---
+++ src/main/java/com/example/apiformatech/ApiFormatechApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiformatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFormatechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFormatechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiformatech/ApiFormatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiformatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFormatechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
