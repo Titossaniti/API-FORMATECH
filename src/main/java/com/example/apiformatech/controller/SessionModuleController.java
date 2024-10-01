9cad53761f636daf82b58b7ccb8fc5ed31f60e16
Titossaniti <tristan.erard@gmail.com>
2024-10-01 15:57:50 +0200

    Path updated for 'session-with-modules' (SessionModuleController)

--- src/main/java/com/example/apiformatech/controller/SessionModuleController.java
+++ src/main/java/com/example/apiformatech/controller/SessionModuleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/session-modules")
+@RequestMapping("/api/session-with-modules")
 public class SessionModuleController {
 
     private final SessionModuleService sessionModuleService;
